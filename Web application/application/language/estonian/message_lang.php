87eecef42ac333c3a42a3cc97e7a92ca15c33e90
Megahit <kalvarseemen@gmail.com>
2016-10-12 22:20:48 +0300

    added translation back

--- Web application/application/language/estonian/message_lang.php
+++ Web application/application/language/estonian/message_lang.php
@@ -1,6 +1,14 @@
 <?php
 
-$lang['page_content'] = 'Lehekülje sisu';
 
+$lang['login_page'] = 'Login leht';
+$lang['front_page'] = 'Esileht';
+$lang['about_page'] = 'Abi';
+$lang['page_content'] = 'Lehekülje sisu';
+$lang['register'] = 'Registreerimine';
+$lang['username'] = 'Kasutajanimi';
+$lang['password'] = 'Parool';
+$lang['login'] = 'Logi sisse';
+$lang['kitchen_menus'] = 'Köögi menüüd';
 
 ?>
--- Web application/application/language/russian/message_lang.php
+++ Web application/application/language/russian/message_lang.php
@@ -1,8 +1,14 @@
 <?php
 
+$lang['login_page'] = 'страница авторизации';
+$lang['front_page'] = 'титульная страница';
+$lang['about_page'] = 'Помогите';
 $lang['page_content'] = 'содержание страницы';
-
-
+$lang['register'] = 'регистр';
+$lang['username'] = 'имя пользователя';
+$lang['password'] = 'пароль';
+$lang['login'] = 'авторизоваться';
+$lang['kitchen_menus'] = 'кухня меню';
 
 
 ?>
