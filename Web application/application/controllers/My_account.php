0bfc6192026b2d1aad9a3c2f445edc50c273735a
Megahit <kalvarseemen@gmail.com>
2016-11-07 01:24:50 +0200

    close #63

--- Web application/application/controllers/My_account.php
+++ Web application/application/controllers/My_account.php
@@ -9,10 +9,11 @@
 			$this->load->model("Change_password_model");
 			$this->Change_password_model->member_page();
 			
-			$this->form_validation->set_rules('cur_pw','Current Password','required');
+			
+			
+			$this->form_validation->set_rules('cur_pw','Current Password','required|callback_change');
 			$this->form_validation->set_rules('new_pw','New Password','required|min_length[8]');
 			$this->form_validation->set_rules('conf_pw','Confirm Password','required|matches[new_pw]');
-			
 			if ( $this->form_validation->run() != true )
 			{
 			$this->load->library('session');
@@ -20,30 +21,36 @@
 			$this->load->view('sidebar');
 			$this->load->view("Change_password");
 			$this->load->view('footer');
-			} else {
+			} else {}
+		}
+			
+		function change() {
+
 			$sql = $this->db->select("*")->from("user")->where("username",$this->session->userdata("username"))->get();
 		
 			foreach($sql->result() as $my_info)
 			{
-				$db_password = $my_info->password;
+				
+				$current_password = $my_info->password;
 				$db_id = $my_info->username;
 			}
-			if ( md5($this->input->post("cur_pw")) == $db_password) {
+			$praegune_parool = $current_password;
+			if ( md5($this->input->post("cur_pw")) == $current_password) {
 				$fixed_pw = md5($this->input->post("new_pw"));
 				$update = $this->db->query("UPDATE `user` SET `password` = '$fixed_pw' WHERE `username` = '$db_id'") or die(mysqli_error());
 				redirect('Passwd_changed',"refresh");
 			} else {
-				echo "Password is incorrect!";
-				$this->load->view("vChangePassword");
+				$this->form_validation->set_message('change', 'Sisestasite vale parooli!');
+				
 			}
 			
 		
 		}
 		
-		
-	}
+	}	
 	
-}
+	
+
 
 
 
