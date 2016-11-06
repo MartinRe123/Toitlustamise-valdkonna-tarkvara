5cb470b5c906b1657192c497280c5d1872b74242
Marvin Maran <marvinmaran100@gmail.com>
2016-11-06 17:40:21 +0200

    some translations

--- Web application/application/language/russian/message_lang.php
+++ Web application/application/language/russian/message_lang.php
@@ -1,30 +1,30 @@
 <?php
 
-$lang['login_page'] = 'страница авторизации';
+$lang['login_page'] = 'Страница авторизации';
 $lang['front_page'] = 'титульная страница';
-$lang['about_page'] = 'Помогите';
+$lang['about_page'] = 'Наверное количество блюда';
 $lang['page_content'] = 'добро пожаловать';
 $lang['register'] = 'регистр';
 $lang['username'] = 'имя пользователя';
 $lang['password'] = 'пароль';
-$lang['password_again'] = 'пароль еще раз';
+$lang['password_again'] = 'пароль ещё раз';
 $lang['login'] = 'авторизоваться';
-$lang['kitchen_menus'] = 'подписка';
-$lang['add_menu'] = 'Добавить новое меню';
-$lang['change_password'] = 'изменение пароля';
+$lang['kitchen_menus'] = 'Меню и заказы';
+$lang['add_menu'] = 'Добаваи новую меню';
+$lang['change_password'] = 'Изменений пароль';
 $lang['archive'] = 'архив';
-$lang['raports'] = 'отчеты';
+$lang['raports'] = 'Рапорты';
 $lang['chat'] = 'чат';
-$lang['email'] = 'электронная почта';
-$lang['role'] = 'роль';
-$lang['admin'] = 'администратор';
-$lang['chef'] = 'повар';
-$lang['department'] = 'отдел';
-$lang['department_name'] = 'название отдела';
+$lang['email'] = 'Электронная почта';
+$lang['role'] = 'Роль';
+$lang['admin'] = 'Администратор';
+$lang['chef'] = 'Повар';
+$lang['department'] = 'Отдел';
+$lang['department_name'] = 'Название отдела';
 $lang['submit'] = 'Отправить';
-$lang['remove_all'] = 'убрать все';
+$lang['remove_all'] = 'Убрать все';
 $lang['kitchen_menu_notification'] = 'Пожалуйста добовляйте на каждую еду менше хотя бы один блюдо.';
 $lang['kitchen_menu_notification2'] = 'На выранное число меню создавляена.';
-$lang['order_notification'] = 'Vigane kogus toidul: '; //umbes et Vigane kogus toidul: <toidu nimi>. see toidu nimi lisatakse hiljem ise.
-$lang['order_notification2'] = 'Tellimuse esitamiseks peab olema valitud vähemalt üks toit.';
+$lang['order_notification'] = 'Наверное количество блюда: '; //umbes et Vigane kogus toidul: <toidu nimi>. see toidu nimi lisatakse hiljem ise.
+$lang['order_notification2'] = 'Для заказа нужна быбрать хотя бы одну блюду..';
 ?>
