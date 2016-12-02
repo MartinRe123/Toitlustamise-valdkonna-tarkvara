b382a1c2b10568b99f207d0db85a60f62813ec12
Marvin Maran <marvinmaran100@gmail.com>
2016-12-02 22:47:01 +0200

    raport view changes

--- Web application/application/views/raport_view.php
+++ Web application/application/views/raport_view.php
@@ -4,13 +4,13 @@
 <div class="content">
 <h1>Raportid</h1>
 <form onsubmit="addDates();" id="search_form" method="post" accept-charset="utf-8" action="">
-<label id="raport_date_from" for="date_from">Algus: </label>
+<label id="raport_date_from" for="date_from"><b>Algus: </b></label>
 <input type="text" id="date_from" value="<?php echo date("Y-m-d"); ?>" name="date_from" readonly="readonly">
 
-<label id="raport_date_to" for="date_to">Lõpp: </label>
+<label id="raport_date_to" for="date_to"><b>Lõpp: </b></label>
 <input type="text" id="date_to" value="<?php echo date("Y-m-d"); ?>" name="date_to" readonly="readonly">
 
-<label id="raport_department" for="department">Osakond: </label>
+<label id="raport_department" for="department"><b>Osakond: </b></label>
 
 
 
@@ -27,13 +27,19 @@
 <input type="submit" value="Otsi">
 </form>
 
-
+<br>
+<?php if($searched){
+		if(!empty($orders)){
+			echo '<input type="button" value="Prindi" onclick="printRaport();"/>'; 
+		}
+}
+?>
 
 <div id="print">
 <?php 
 	if($searched){
 		if(!empty($orders)){
-			echo '<input type="button" value="Prindi" onclick="printRaport();"/>';
+			
 			foreach($orders as $order){
 				echo '<div class="raport_order_info">';
 				$date = $order['date'];
@@ -49,15 +55,14 @@
 				
 				echo '<h2>'.$section_name.' ('.$date.')</h2>';
 				$max_rows = max(count($breakfast_array), count($lunch_array), count($supper_array));
-				echo 'MAX: '.$max_rows.'<br>';
-				echo '<table><tr><td>Hommikusöök</td><td>Lõunasöök</td><td>Õhtusöök</td></tr><tr>';
+				echo '<table class="raport_view"><tr><th>Hommikusöök</th><th>Kogus</th><th>Lõunasöök</th><th>Kogus</th><th>Õhtusöök</th><th>Kogus</th></tr><tr>';
 				for ($i = 0; $i < $max_rows; $i++){
-					echo '<tr>';
+					echo '<tr >';
 					if($i < count($breakfast_array)){
 						$b_inf = explode("=", $breakfast_array[$i]);
 						if(count($b_inf) > 1){
-							echo 'Test: '.count($breakfast_array).'    '.$i.'<br>';
-							echo '<td><span title="'.$b_inf[1].'">'.$b_inf[0].'</span> (Kogus: '.$b_inf[2].')</td>';
+							echo '<td class="raport_view"><span title="'.$b_inf[1].'">'.$b_inf[0].'</td>';
+							echo '<td class="raport_view2"></span> '.$b_inf[2].'</td>';
 						}else{
 							echo '<td></td>';
 						}
@@ -67,8 +72,8 @@
 					if($i < count($lunch_array)){
 						$l_inf = explode("=", $lunch_array[$i]);
 						if(count($l_inf) > 1){
-							echo 'Test: '.count($lunch_array).'    '.$i.'<br>';
-							echo '<td><span title="'.$l_inf[1].'">'.$l_inf[0].'</span> (Kogus: '.$l_inf[2].')</td>';
+							echo '<td class="raport_view"><span title="'.$l_inf[1].'">'.$l_inf[0].'</td>';
+							echo '<td class="raport_view2"></span> '.$l_inf[2].'</td>';
 						}else{
 							echo '<td></td>';
 						}
@@ -78,8 +83,8 @@
 					if($i < count($supper_array)){
 						$s_inf = explode("=", $supper_array[$i]);
 						if(count($s_inf) > 1){
-							echo 'Test: '.count($supper_array).'    '.$i.'<br>';
-							echo '<td><span title="'.$s_inf[1].'">'.$s_inf[0].'</span> (Kogus: '.$s_inf[2].')</td>';
+							echo '<td class="raport_view"><span title="'.$s_inf[1].'">'.$s_inf[0].'</td>';
+							echo '<td class="raport_view2"></span> '.$s_inf[2].'</td>';
 						}else{
 							echo '<td></td>';
 						}
@@ -90,10 +95,9 @@
 				}
 				echo '</table>';
 				if($comments){
-					echo '<p>Lisamärkused:</p>';
-					echo '<p>'.$comments.'</p>';
+					echo '<p><b>Lisamärkused:  </b>'.$comments.'</p>';
 				}
-				echo '<br/><hr><br/><br/></div>';
+				echo '<br/><hr><br/></div>';
 			}
 		}else{
 			echo 'Selles ajavahemikus ei leitud ühtegi tellimust.';
